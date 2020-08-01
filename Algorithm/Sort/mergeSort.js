44b41349f86adbe4bdb7ede5f9c23f5312a92dc5
JiSop <jisop.shin@gmail.com>
2020-08-01 16:43:50 +0900

    Update mergeSort

--- Algorithm/Sort/mergeSort.js
+++ Algorithm/Sort/mergeSort.js
@@ -1,3 +1,18 @@
+/**
+ * **병합 정렬**
+ *
+ * 재귀적으로 리스트를 절반으로 잘라 분할 정복하여 정렬한다.
+ * 리스트의 길이가 1 이하이면 이미 정렬된 것으로 본다.
+ * - 시간 복잡도: O(n log n)
+ * - 공간 복잡도: O(n)
+ * - 안정 정렬
+ *
+ * 1. 분할 : 정렬되지 않은 리스트를 절반으로 잘라 비슷한 크기의 두 리스트로 나눈다.
+ * 2. 정복 : 각 부분 리스트를 재귀적으로 합병 정렬을 이용해 정렬한다.
+ * 3. 결합 : 두 부분 리스트를 다시 하나의 정렬된 리스트로 합병한다.
+ *    - 이때 정렬 결과가 임시배열에 저장된다.
+ * 4. 복사 : 임시 배열에 저장된 결과를 원래 배열에 복사한다.
+ */
 function merge(left, right) {
   let result = [];
   while (left.length && right.length) {
@@ -15,9 +30,8 @@ function merge(left, right) {
   }
   return result;
 }
-
 function mergeSort(arr) {
-  if (arr.length <= 1) return [...arr];
+  if (arr.length <= 1) return arr;
   const mid = Math.floor(arr.length / 2);
   const left = arr.slice(0, mid);
   const right = arr.slice(mid);
